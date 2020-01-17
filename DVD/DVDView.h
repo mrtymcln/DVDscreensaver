cee042b96c3bea7f091d50c7b0a9c02329b0a5d6
Marty McLean <martymclean@icloud.com>
2020-01-17 11:20:30 +1100

    Initial Commit

---
+++ DVD/DVDView.h
@@ -0,0 +1,13 @@
+//
+//  DVDView.h
+//  DVD
+//
+//  Created by Marty McLean on 17/1/20.
+//  Copyright © 2020 Marty McLean. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DVDView : ScreenSaverView
+
+@end
